9ac99fecf81d48e0de0d2fe5ff450f519ffa8905
sssmi <simeon.kerkola@gmail.com>
2018-07-24 12:36:37 +0300

    Add Clojure demo

--- demo/clojure.cljs
+++ demo/clojure.cljs
@@ -1,10 +1,42 @@
 (ns example
   (:require [reagent.core :as r]))
-(def click-count (r/atom 0))
 
-(defn counting-component []
+(defn home-page []
   [:div
-   "The atom " [:code "click-count"] " has value: "
-   @click-count ". "
-   [:input {:type "button" :value "Click me!"
-            :on-click #(swap! click-count inc)}]])
+    [board state]
+    [:div [:a {:href "/about"} "go to about page"]]])
+
+(defn about-page []
+    [:div [:h2 "About reagent-fiddle"]
+      [:div [:a {:href "/"} "go to the home page"]]])
+
+;; -------------------------
+;; Routes
+
+(defonce page (atom #'home-page))
+
+(defn current-page []
+  [:div [@page]])
+
+(secretary/defroute "/" []
+                    (reset! page #'home-page))
+
+(secretary/defroute "/about" []
+                    (reset! page #'about-page))
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (r/render [current-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (accountant/configure-navigation!
+    {:nav-handler
+      (fn [path]
+        (secretary/dispatch! path)
+        :path-exists?)
+      (fn [path]
+        (secretary/locate-route path))})
+  (accountant/dispatch-current!)
+  (mount-root))
